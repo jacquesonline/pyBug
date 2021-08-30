1e6168e119285186cc6357dfb838437b09e2c541
Your Name <you@example.com>
2021-08-30 18:48:53 +1000

    Sync 30/08/2021

--- binary_search1.py
+++ binary_search1.py
@@ -1,21 +1,32 @@
-import re
-def compare_strings(string1, string2):
-  #Convert both strings to lowercase 
-  #and remove leading and trailing blanks
-  string1 = string1.lower().strip()
-  string2 = string2.lower().strip()
+def find_item(list, item):
 
-  #Ignore punctuation
-  punctuation = r"[.?!,;:\-']"
-  string1 = re.sub(punctuation, r"", string1)
-  string2 = re.sub(punctuation, r"", string2)
+  #Sort the list
+  list.sort()
 
-  #DEBUG CODE GOES HERE
-  # print(___)
+  #Returns True if the item is in the list, False if not.
+  if len(list) == 0:
+    return False
 
-  return string1 == string2
+  #Is the item in the center of the list?
+  middle = len(list)//2
+  if list[middle] == item:
+    return True
 
-print(compare_strings("Have a Great Day!", "Have a great day?")) # True
-print(compare_strings("It's raining again.", "its raining, again")) # True
-print(compare_strings("Learn to count: 1, 2, 3.", "Learn to count: one, two, three.")) # False
-print(compare_strings("They found some body.", "They found somebody.")) # False
+  #Is the item in the first half of the list? 
+  if item < list[middle]:
+      
+    #Call the function with the first half of the list
+    return find_item(list[:middle], item)
+  else:
+    #Call the function with the second half of the list
+    return find_item(list[middle+1:], item)
+
+  return False
+
+#Do not edit below this line - This code helps check your work!
+list_of_names = ["Parker", "Drew", "Cameron", "Logan", "Alex", "Chris", "Terry", "Jamie", "Jordan", "Taylor"]
+
+print(find_item(list_of_names, "Alex")) # True
+print(find_item(list_of_names, "Andrew")) # False
+print(find_item(list_of_names, "Drew")) # True
+print(find_item(list_of_names, "Jared")) # False
